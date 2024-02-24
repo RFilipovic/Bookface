f2a9522a086d9d1a83a5007fc85fc8ab18de0119
Rene <rene.filipovic03@gmail.com>
2024-02-24 14:22:42 +0100

    added impl class for JWT

---
+++ src/main/java/com/bookface/comms/security/JwtAuthenticationFilter.java
@@ -0,0 +1,28 @@
+package com.bookface.comms.security;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class JwtAuthenticationFilter extends OncePerRequestFilter {
+
+    private final JwtTokenUtil jwtTokenUtil;
+
+    public JwtAuthenticationFilter(JwtTokenUtil jwtTokenUtil){
+        this.jwtTokenUtil = jwtTokenUtil;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String header = request.getHeader("Authorization");
+
+        if(header != null && header.startsWith("Bearer ")){
+            String token = header.substring(7);
+            String username = jwtTokenUtil.extractUsername(token);
+        }
+    }
+}
